2006357e22603ed52502a2a602ebd314020c32db
Rob B <computerguy440+gh@gmail.com>
2024-10-13 22:27:26 -0400

    Stop tracking generated files that should have been gitignored

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-//front end TS zone
-import App from './App.svelte';
-const app = new App({
-    target: document.getElementById('app'),
-});
-export default app;
-//# sourceMappingURL=main.js.map
--- dist/src/main.js
+++
@@ -1,7 +0,0 @@
-//front end TS zone
-import App from './App.svelte';
-const app = new App({
-    target: document.body,
-});
-export default app;
-//# sourceMappingURL=main.js.map
