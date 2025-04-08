a105c22d8febffe73e8c6ce8a239d99b4815fa5c
kemvolt <kemvolt@gmail.com>
2025-04-08 14:10:45 -0400

    Slight changes to view buttons
    -removed overflow
    -swapped delete and show tree places

--- src/sveltePages/View.svelte
+++ src/sveltePages/View.svelte
@@ -119,6 +119,7 @@
   }
 </script>
 
+
 <!-- Action display above everything -->
 <ActionDisplay />
 
@@ -184,22 +185,6 @@
             /></svg
           >
         </button>
-        <!--Delete button-->
-        <button
-          class="warn-button center-button-icons flex-grow font-semibold shadow"
-          on:click={() => deleteDialog?.showModal()}
-        >
-          <svg
-            class="icon-small"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            ><path
-              fill="#ffffff"
-              d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
-            /></svg
-          >
-        </button>
-
         <!-- Add Show Item Tree button when tree is hidden -->
         {#if !showItemTree}
           <button
@@ -217,6 +202,21 @@
             >
           </button>
         {/if}
+        <!--Delete button-->
+        <button
+          class="warn-button center-button-icons flex-grow font-semibold shadow"
+          on:click={() => deleteDialog?.showModal()}
+        >
+          <svg
+            class="icon-small"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            ><path
+              fill="#ffffff"
+              d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
+            /></svg
+          >
+        </button>
       </div>
     </Window>
 
