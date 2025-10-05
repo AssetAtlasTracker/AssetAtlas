0774d692153ceb554acd58ecc925493e5be26ede
Emile Marois <maroisem@rose-hulman.edu>
2025-10-05 11:56:29 -0400

    Update package-lock.json
    Update package-lock.json

--- migrate-mongo-config.cjs
+++ migrate-mongo-config.cjs
@@ -2,10 +2,8 @@
 
 const config = {
   mongodb: {
-    // TODO Change (or review) the url to your MongoDB:
     url: "mongodb://localhost:27017",
 
-    // TODO Change this to your database name:
     databaseName: "assetatlas_db",
 
     options: {
--- migrations/20251001150825-test_migration.cjs
+++ migrations/20251001150825-test_migration.cjs
@@ -5,10 +5,6 @@ module.exports = {
    * @returns {Promise<void>}
    */
   async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
     await db.collection('customfields').insertOne({ fieldName: "Test Field", dataType: "String", __v: 0 });
   },
 
@@ -18,9 +14,6 @@ module.exports = {
    * @returns {Promise<void>}
    */
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
     await db.collection('customfields').deleteOne({ fieldName: "Test Field" });
   }
 };
