1844d99922451b79723e33201216662fb7d18788
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2025-10-28 17:29:22 -0400

    updated linting rules
    - lint files in tests folder
    - lint svelte files
    - use tabs instead of spaces

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import sveltePlugin from "eslint-plugin-svelte";
+import globals from "globals";
+import svelteParser from "svelte-eslint-parser";
 
 export default [
   {
@@ -22,13 +24,41 @@ export default [
       "@typescript-eslint": tseslint,
     },
     rules: {
-      //"no-unused-vars": "warn",
-      ...pluginJs.configs.recommended.rules, 
+      ...pluginJs.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": [
-    "warn",//dont worry about it. look at me. dont worry about it.
-    { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }
-  ],
+        "warn",
+        { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }
+      ],
+      "indent": ["warn", "tab"]
+    },
+  },
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      svelte: sveltePlugin,
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...sveltePlugin.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "svelte/indent": ["warn", { "indent": "tab" }]
     },
   },
 ];
