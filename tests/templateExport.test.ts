b67dd6d7306c89f1a216c269baee09ae32c9c5a7
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2025-10-29 14:59:35 -0400

    convert more spaces to tabs

--- tests/templateExport.test.ts
+++ tests/templateExport.test.ts
@@ -5,32 +5,32 @@ import { CSVFormatterPopulated } from "../src/utility/formating/CSVFormatterPopu
 
 describe("Testing Template Formatting", () => {
 
-    it("Should format one template", async () => {
-        let firstTemplate = new Template() as unknown as ITemplatePopulated;
-        firstTemplate.name = "first";
-        firstTemplate.id = 1;
-        firstTemplate.name = "first";
-        let nameField = new CustomField();
-        nameField.id = 2;
-        nameField.fieldName = "name";
-        nameField.dataType = "string";
-        let amountField = new CustomField();
-        amountField.id = 3;
-        amountField.fieldName = "amount";
-        amountField.dataType = "number";
-        let edibleField = new CustomField();
-        edibleField.id = 4;
-        edibleField.fieldName = "edible";
-        edibleField.dataType = "boolean";
+	it("Should format one template", async () => {
+		let firstTemplate = new Template() as unknown as ITemplatePopulated;
+		firstTemplate.name = "first";
+		firstTemplate.id = 1;
+		firstTemplate.name = "first";
+		let nameField = new CustomField();
+		nameField.id = 2;
+		nameField.fieldName = "name";
+		nameField.dataType = "string";
+		let amountField = new CustomField();
+		amountField.id = 3;
+		amountField.fieldName = "amount";
+		amountField.dataType = "number";
+		let edibleField = new CustomField();
+		edibleField.id = 4;
+		edibleField.fieldName = "edible";
+		edibleField.dataType = "boolean";
 
-        firstTemplate.fields.push(nameField);
-        firstTemplate.fields.push(amountField);
-        firstTemplate.fields.push(edibleField);
+		firstTemplate.fields.push(nameField);
+		firstTemplate.fields.push(amountField);
+		firstTemplate.fields.push(edibleField);
 
-        const csvContent = `template name\nfirst,name,amount,edible\n,string,number,boolean`;
-        const formatter = new CSVFormatterPopulated([], [firstTemplate], [], []);
-        const str = formatter.formatTemplates();
-        expect(str).toBe(csvContent);
-    });
+		const csvContent = `template name\nfirst,name,amount,edible\n,string,number,boolean`;
+		const formatter = new CSVFormatterPopulated([], [firstTemplate], [], []);
+		const str = formatter.formatTemplates();
+		expect(str).toBe(csvContent);
+	});
 
 });
--- tests/templateParser.test.ts
+++ tests/templateParser.test.ts
@@ -7,51 +7,51 @@ const resources = test();
 const CSVTemplateParser = resources.templateParser;
 
 describe("Testing Template Parsing", () => {
-    it("Should make one template with all types of fields", async () => {
-        const csvContent = `template name\nfirst,name,amount,edible\n,string,number,boolean`;
+	it("Should make one template with all types of fields", async () => {
+		const csvContent = `template name\nfirst,name,amount,edible\n,string,number,boolean`;
 
-        const templateParser = new CSVTemplateParser();
-        const canParse = templateParser.canParse(csvContent.split(/\n|\r/)[0].split(/,/));
-        expect(canParse).toBeTruthy();
-        templateParser.parse(csvContent);
+		const templateParser = new CSVTemplateParser();
+		const canParse = templateParser.canParse(csvContent.split(/\n|\r/)[0].split(/,/));
+		expect(canParse).toBeTruthy();
+		templateParser.parse(csvContent);
 
-        let firstTemplate = new Template();
-        firstTemplate.id = 1;
-        firstTemplate.name = "first";
-        let nameField = new CustomField();
-        nameField.id = 2;
-        nameField.fieldName = "name";
-        nameField.dataType = "string";
-        let amountField = new CustomField();
-        amountField.id = 3;
-        amountField.fieldName = "amount";
-        amountField.dataType = "number";
-        let edibleField = new CustomField();
-        edibleField.id = 4;
-        edibleField.fieldName = "edible";
-        edibleField.dataType = "boolean";
+		let firstTemplate = new Template();
+		firstTemplate.id = 1;
+		firstTemplate.name = "first";
+		let nameField = new CustomField();
+		nameField.id = 2;
+		nameField.fieldName = "name";
+		nameField.dataType = "string";
+		let amountField = new CustomField();
+		amountField.id = 3;
+		amountField.fieldName = "amount";
+		amountField.dataType = "number";
+		let edibleField = new CustomField();
+		edibleField.id = 4;
+		edibleField.fieldName = "edible";
+		edibleField.dataType = "boolean";
 
-        firstTemplate.fields.push(nameField.id);
-        firstTemplate.fields.push(amountField.id);
-        firstTemplate.fields.push(edibleField.id);
+		firstTemplate.fields.push(nameField.id);
+		firstTemplate.fields.push(amountField.id);
+		firstTemplate.fields.push(edibleField.id);
 
-        let fieldMap = new Map<Types.ObjectId, ICustomField>();
-        fieldMap.set(nameField.id, nameField);
-        fieldMap.set(amountField.id, amountField);
-        fieldMap.set(edibleField.id, edibleField);
+		let fieldMap = new Map<Types.ObjectId, ICustomField>();
+		fieldMap.set(nameField.id, nameField);
+		fieldMap.set(amountField.id, amountField);
+		fieldMap.set(edibleField.id, edibleField);
 
-        const templateToTest = templateParser.templatesToAdd[0];
-        const fieldsToTest = templateParser.customFieldMap;
-        expect(templateToTest.name).toBe(firstTemplate.name);
-        for (var i = 0; i < templateToTest.fields.length; i++) {
-            const fieldId = firstTemplate.fields[i];
-            const field = fieldMap.get(fieldId.toHexString() as unknown as Types.ObjectId);
-            expect(field).not.toBe(undefined);
-            const testId = templateToTest.fields[i];
-            const testField = fieldsToTest.get(testId.toHexString() as unknown as Types.ObjectId);
-            expect(testField).not.toBe(undefined);
-            expect(field!.fieldName).toBe(testField!.fieldName);
-            expect(field!.dataType).toBe(testField!.dataType);
-        }
-    });
+		const templateToTest = templateParser.templatesToAdd[0];
+		const fieldsToTest = templateParser.customFieldMap;
+		expect(templateToTest.name).toBe(firstTemplate.name);
+		for (var i = 0; i < templateToTest.fields.length; i++) {
+			const fieldId = firstTemplate.fields[i];
+			const field = fieldMap.get(fieldId.toHexString() as unknown as Types.ObjectId);
+			expect(field).not.toBe(undefined);
+			const testId = templateToTest.fields[i];
+			const testField = fieldsToTest.get(testId.toHexString() as unknown as Types.ObjectId);
+			expect(testField).not.toBe(undefined);
+			expect(field!.fieldName).toBe(testField!.fieldName);
+			expect(field!.dataType).toBe(testField!.dataType);
+		}
+	});
 });
