562d1f9c964affad79edbff84bf4a8861c0dade7
Nathan Yang <njy0809@gmail.com>
2025-10-30 11:50:22 -0400

    about and users page

---
+++ src/routes/users/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import UserList from "$lib/components/UserList.svelte";
+  import TopBar from "$lib/components/TopBar.svelte";
+  import Menu from "$lib/components/Menu.svelte";
+  import ActionDisplay from "$lib/components/ActionDisplay.svelte";
+  import "$lib/styles/main.css";
+
+  let menu: HTMLDialogElement;
+  let searchQuery = "";
+  
+  function onSearch(query: string) {
+    //maybe some day
+  }
+
+  console.log("Users.svelte is loading");
+</script>
+
+<ActionDisplay />
+
+<TopBar searchQuery={""} {onSearch} {menu}></TopBar>
+
+<div class="page-with-topbar">
+  <Menu bind:menu />
+  <UserList />
+</div>
