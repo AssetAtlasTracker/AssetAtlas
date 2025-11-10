80edc3c530ca13c96f42aa33721453cc2df40950
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2025-11-10 15:21:59 -0500

    display git commit hash on version page

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,38 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { sveltePreprocess } from 'svelte-preprocess'
+import { execSync } from 'node:child_process';
 
 const production = process.env.NODE_ENV === 'production';
 
+const gitVer = (() => {
+	try {
+		return execSync('git describe --always --dirty', { encoding: 'utf8' }).trim();
+	} catch {
+		return 'dev';
+	}
+})();
+
 export default defineConfig({
-  root: 'src',
-  plugins: [
-    svelte({
-      preprocess: sveltePreprocess(),
-      compilerOptions: {
-        dev: !production
-      }
-    })
-  ],
-  build: {
-    outDir: '../dist', //Outputs files one level up at project root dist folder
-    sourcemap: !production,
-    emptyOutDir: false
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 3001,
-  }
+	root: 'src',
+	plugins: [
+		svelte({
+			preprocess: sveltePreprocess(),
+			compilerOptions: {
+				dev: !production
+			}
+		})
+	],
+	build: {
+		outDir: '../dist', //Outputs files one level up at project root dist folder
+		sourcemap: !production,
+		emptyOutDir: false
+	},
+	server: {
+		host: "0.0.0.0",
+		port: 3001,
+	},
+	define: {
+		'import.meta.env.APP_VERSION': JSON.stringify(process.env.APP_VERSION ?? gitVer),
+	}
 });
