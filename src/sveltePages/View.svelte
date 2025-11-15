fd22a58a70c14edb04a262c42e64c428fe617286
Emile Marois <maroisem@rose-hulman.edu>
2025-11-15 12:08:26 -0500

    Added edit on login to item view

--- src/sveltePages/View.svelte
+++ src/sveltePages/View.svelte
@@ -19,6 +19,8 @@
   import EditItem from "../svelteComponents/EditItem.svelte";
   import MoveItem from "../svelteComponents/MoveItem.svelte";
   import Window from "../svelteComponents/Window.svelte";
+  import {login, getEditOnLogin} from '../stores/loginStore.js';
+  import type { LoginState } from "../stores/loginStore.js";
   export let params: { id?: string };
 
   let editDialog: HTMLDialogElement | undefined;
@@ -30,13 +32,18 @@
   let moveDialog: HTMLDialogElement | undefined;
   let menu: HTMLDialogElement | undefined;
 
+  let currentLogin: LoginState | undefined;
+	login.subscribe((value) => {
+		currentLogin = value;
+	});
+
   let unique = {};
   function restart() {
     unique = {};
   }
 
   let showItemTree = true;
-
+  
   function handleTreeClose() {
     showItemTree = false;
   }
@@ -160,20 +167,24 @@
           >
         </button>
         <!--Edit button-->
-        <button
-          class="border-button center-button-icons flex-grow font-semibold shadow"
-          on:click={() => editDialog?.showModal()}
-        >
-          <svg
-            class="icon-small"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            ><path
-              fill="#ffffff"
-              d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"
-            /></svg
+
+        {#if !getEditOnLogin() || currentLogin?.isLoggedIn}
+          <button
+            class="border-button center-button-icons flex-grow font-semibold shadow"
+            on:click={() => editDialog?.showModal()}
           >
-        </button>
+            <svg
+              class="icon-small"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              ><path
+                fill="#ffffff"
+                d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"
+              /></svg
+            >
+          </button>
+        {/if}
+        
         <!-- Add Show Item Tree button when tree is hidden -->
         {#if !showItemTree}
           <button
