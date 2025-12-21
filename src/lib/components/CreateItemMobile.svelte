0376dea5ef989c50ecc0ef6f6cd79de9407b12e6
Nathan Yang <njy0809@gmail.com>
2025-12-21 12:39:43 -0500

    front end complete

--- src/lib/components/CreateItemMobile.svelte
+++ src/lib/components/CreateItemMobile.svelte
@@ -5,12 +5,13 @@
 	import ImageSelector from "./ImageSelector.svelte";
 	import InfoToolTip from "./InfoToolTip.svelte";
 	import { Switch } from "@skeletonlabs/skeleton-svelte";
+	import { CameraIcon } from '@lucide/svelte';
+	import { FileUpload } from '@skeletonlabs/skeleton-svelte';
     import { 
 		createItemState,
 		handleCreateItem, 
 		initializeItemEdit, 
-		resetForm, 
-		setDialog,
+		resetForm,
 		handleParentItemInput,
 		handleHomeItemInput,
 		handleTemplateInput,
@@ -29,7 +30,6 @@
         setOnItemCreated
 	} from "$lib/stores/createItemStore.svelte";
     import { createEventDispatcher } from "svelte";
-    import type { IBasicItemPopulated } from "$lib/server/db/models/basicItem";
 
 	export let dialog: HTMLDialogElement;
 	export let duplicate = false;
@@ -62,6 +62,32 @@
 	<div class="page-component large-dialog-internal">
 		<form on:submit|preventDefault={handleCreateItem}>
 			<div class="flex flex-col space-y-4">
+				<div class="flex flex-col space-y-2">
+					<!-- <ImageSelector on:imageChange={handleImageChange} /> -->
+					<FileUpload>
+						<FileUpload.Dropzone>
+							<CameraIcon class="size-32" />
+							<span>Upload Image</span>
+							<FileUpload.Trigger>Browse Files</FileUpload.Trigger>
+							<FileUpload.HiddenInput />
+						</FileUpload.Dropzone>
+						<FileUpload.ItemGroup>
+							<FileUpload.Context>
+								{#snippet children(fileUpload)}
+									{#each fileUpload().acceptedFiles as file (file.name)}
+										<FileUpload.Item {file}>
+											<FileUpload.ItemName>{file.name}</FileUpload.ItemName>
+											<FileUpload.ItemSizeText>{file.size} bytes</FileUpload.ItemSizeText>
+											<FileUpload.ItemDeleteTrigger />
+										</FileUpload.Item>
+									{/each}
+								{/snippet}
+							</FileUpload.Context>
+						</FileUpload.ItemGroup>
+						<FileUpload.ClearTrigger>Clear Files</FileUpload.ClearTrigger>
+					</FileUpload>
+				</div>
+
 				<div class="flex space-x-4">
 					<!-- Name -->
 					<label class="flex-column flex-grow">
@@ -73,7 +99,9 @@
 							bind:value={createItemState.name}
 							required />
 					</label>
+				</div>
 
+				<div class="flex space-x-4">
 					<!-- Tags -->
 					<label class="flex-column flex-grow">
 						Tags:
@@ -84,23 +112,6 @@
 							bind:value={createItemState.tags}></textarea>
 					</label>
 				</div>
-
-				<!-- Description -->
-				<label class="min-w-[400px]">
-					Description:
-					<textarea
-						rows="4"
-						id="resize-none-textarea"
-						class="dark-textarea py-2 px-4 w-full"
-						placeholder="My medium-sized, red toolbox"
-						bind:value={createItemState.description}></textarea>
-				</label>
-
-				<br />
-				<div class="flex flex-col space-y-2">
-					<ImageSelector on:imageChange={handleImageChange} />
-				</div>
-				<br />
 
 				<Switch
 					checked={createItemState.sameLocations}
@@ -114,6 +125,39 @@
 						>Item is currently at its home location</Switch.Label>
 					<Switch.HiddenInput />
 				</Switch>
+
+				<div class="flex space-x-4">
+					<!-- Home Item -->
+					<label class="flex-column flex-grow relative">
+						<div class="flex items-center gap-2">
+							<span>Home Location:</span>
+							<InfoToolTip
+								message="Where an item should normally be, e.g a shirt's home item may be a drawer." />
+						</div>
+						<input
+							type="text"
+							class="dark-textarea py-2 px-4 w-full"
+							bind:value={createItemState.homeItemName}
+							on:input={handleHomeItemInput}
+							on:focus={handleHomeItemFocus}
+							on:blur={() => (createItemState.homeItemSuggestions = [])} />
+						{#if createItemState.homeItemSuggestions.length > 0}
+							<ul class="suggestions suggestion-box">
+								{#each createItemState.homeItemSuggestions as item (item.id)}
+									<button
+										class="suggestion-item"
+										type="button"
+										on:mousedown={(e) => {
+											e.preventDefault();
+											selectHomeItem(item);
+										}}>
+										{item.name}
+									</button>
+								{/each}
+							</ul>
+						{/if}
+					</label>
+				</div>
 
 				<div class="flex space-x-4">
 					<!-- Parent Item -->
@@ -148,47 +192,36 @@
 							{/if}
 						</label>
 					{/if}
-
-					<!-- Home Item -->
-					<label class="flex-column flex-grow relative">
-						<div class="flex items-center gap-2">
-							<span>Home Location:</span>
-							<InfoToolTip
-								message="Where an item should normally be, e.g a shirt's home item may be a drawer." />
-						</div>
-						<input
-							type="text"
-							class="dark-textarea py-2 px-4 w-full"
-							bind:value={createItemState.homeItemName}
-							on:input={handleHomeItemInput}
-							on:focus={handleHomeItemFocus}
-							on:blur={() => (createItemState.homeItemSuggestions = [])} />
-						{#if createItemState.homeItemSuggestions.length > 0}
-							<ul class="suggestions suggestion-box">
-								{#each createItemState.homeItemSuggestions as item (item.id)}
-									<button
-										class="suggestion-item"
-										type="button"
-										on:mousedown={(e) => {
-											e.preventDefault();
-											selectHomeItem(item);
-										}}>
-										{item.name}
-									</button>
-								{/each}
-							</ul>
-						{/if}
-					</label>
 				</div>
 				<br />
+
+				<!-- Description -->
+				<label class="min-w-[400px]">
+					Description:
+					<textarea
+						rows="4"
+						id="resize-none-textarea"
+						class="dark-textarea py-2 px-4 w-full"
+						placeholder="My medium-sized, red toolbox"
+						bind:value={createItemState.description}></textarea>
+				</label>
 
 				<!-- Template Field and Create Template Button -->
 				<div class="flex space-x-4 items-center">
 					<label class="flex-column flex-grow relative">
-						<div class="flex items-center gap-2">
-							<span>Template:</span>
-							<InfoToolTip
-								message="A template is a more narrow category of similar items that share common fields. Select an existing template or create a new one." />
+						<div class="flex items-center justify-between w-full">
+							<div class="flex items-center gap-2">
+								<span>Template:</span>
+								<InfoToolTip
+									message="A template is a more narrow category of similar items that share common fields. Select an existing template or create a new one." />
+							</div>
+							
+							<button
+								type="button"
+								class="border-button font-semibold shadow"
+								on:click={() => (showCreateTemplateDialog = true)}>
+								Create New Template
+							</button>
 						</div>
 						<input
 							type="text"
@@ -213,29 +246,19 @@
 							</ul>
 						{/if}
 					</label>
-					<div>
-						<br />
-						<button
-							type="button"
-							class="border-button font-semibold shadow"
-							on:click={() => (showCreateTemplateDialog = true)}>
-							Create New Template
-						</button>
-					</div>
 				</div>
 			</div>
 			<br />
 
 			<!-- Custom Fields -->
-			<div class="simple-flex px-2">
-				<h2 class="font-bold text-lg">Custom Fields</h2>
-				<button
+			<h2 class="font-bold text-lg">Custom Fields</h2>
+				
+			<button
 					type="button"
 					class="border-button font-semibold shadow small-add-button"
 					on:click={addCustomFieldLine}>
 					+
-				</button>
-			</div>
+			</button>
 			{#each createItemState.customFields as field, index (field.fieldId)}
 				<CustomFieldPicker
 					bind:field
