941eb85552e6d734e53b7bb8ae6a004a1a52a1f0
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2026-01-20 20:05:37 -0500

    replaced SVGs with Lucide icons

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -3,6 +3,11 @@
 	import { dragDropMode } from "$lib/stores/dragDropStore.js";
 	import { topBarHeight } from "$lib/stores/topBarStore.js";
 	import { bringToFront } from "$lib/stores/zIndexStore.js";
+	import {
+		ExternalLinkIcon,
+		FoldVerticalIcon,
+		UnfoldVerticalIcon,
+	} from "lucide-svelte";
 	import { createEventDispatcher, onDestroy, onMount } from "svelte";
 
 	export let initialX = 0;
@@ -154,8 +159,8 @@
 		}
 
 		bringWindowToFront();
-  }
-  
+	}
+
 	function handleKeyDown(event: KeyboardEvent) {
 		if (
 			event.key === "Escape" &&
@@ -230,9 +235,9 @@
 			on:mousedown={handleMouseDown}
 			on:keydown={(e) =>
 				e.key === "Enter" &&
-					handleMouseDown(
-						new MouseEvent("mousedown", { clientX: 0, clientY: 0 }),
-					)}
+				handleMouseDown(
+					new MouseEvent("mousedown", { clientX: 0, clientY: 0 }),
+				)}
 			role="button"
 			tabindex="0"
 			aria-label="Drag to move window"
@@ -256,48 +261,10 @@
 							? 'Expand'
 							: 'Collapse'} this window">
 						{#if !isCollapsed}
-							<svg
-								viewBox="0 0 14 14"
-								width="14"
-								height="14"
-								xmlns="http://www.w3.org/2000/svg"
-								><g style="fill:#e2e2e2;fill-opacity:1"
-									><path
-										class="st0"
-										d="M36.6 256c0-10.1 8.2-18.3 18.3-18.3h402.3c10.1 0 18.3 8.2 18.3 18.3s-8.2 18.3-18.3 18.3H54.9c-10.1 0-18.3-8.2-18.3-18.3M256 0c10.1 0 18.3 8.2 18.3 18.3v164.6c0 10.1-8.2 18.3-18.3 18.3s-18.3-8.2-18.3-18.3V18.3C237.7 8.2 245.9 0 256 0"
-										style="fill:#e2e2e2;fill-opacity:1"
-										transform="matrix(0 .0319 -.02734 0 14 -1.167)" /><path
-										class="st0"
-										d="M342.1 96.8c7.1 7.1 7.2 18.7 0 25.9L269 195.8c-7.1 7.1-18.7 7.2-25.9 0L170 122.7c-7.1-7.2-7.1-18.7 0-25.9 7.2-7.1 18.7-7.1 25.9 0L256 157l60.2-60.2c7.1-7.2 18.7-7.2 25.9 0M256 512c10.1 0 18.3-8.2 18.3-18.3V329.1c0-10.1-8.2-18.3-18.3-18.3s-18.3 8.2-18.3 18.3v164.6c0 10.1 8.2 18.3 18.3 18.3"
-										style="fill:#e2e2e2;fill-opacity:1"
-										transform="matrix(0 .0319 -.02734 0 14 -1.167)" /><path
-										class="st0"
-										d="M342.1 415.2c7.1-7.1 7.2-18.7 0-25.9L269 316.2c-7.1-7.1-18.7-7.2-25.9 0L170 389.3c-7.1 7.2-7.1 18.7 0 25.9 7.2 7.1 18.7 7.1 25.9 0L256 355l60.2 60.2c7.1 7.2 18.7 7.2 25.9 0"
-										style="fill:#e2e2e2;fill-opacity:1"
-										transform="matrix(0 .0319 -.02734 0 14 -1.167)" /></g
-								></svg>
+							<FoldVerticalIcon />
 						{/if}
 						{#if isCollapsed}
-							<svg
-								viewBox="0 0 14 14"
-								width="14"
-								height="14"
-								xmlns="http://www.w3.org/2000/svg"
-								><g style="fill:#e2e2e2;fill-opacity:1"
-									><path
-										class="st0"
-										d="M36.6 256c0-10.1 8.2-18.3 18.3-18.3h402.2c10.1 0 18.3 8.2 18.3 18.3s-8.2 18.3-18.3 18.3H54.9c-10.1 0-18.3-8.2-18.3-18.3M256 201.2c10.1 0 18.3-8.2 18.3-18.3V18.3C274.3 8.2 266.1 0 256 0s-18.3 8.2-18.3 18.3v164.5c0 10.2 8.2 18.4 18.3 18.4"
-										style="fill:#e2e2e2;fill-opacity:1"
-										transform="matrix(0 .0319 -.02735 0 14 -1.168)" /><path
-										class="st0"
-										d="M342.1 104.4c7.1-7.1 7.2-18.7 0-25.9L269 5.4c-7.1-7.1-18.7-7.2-25.9 0L170 78.5c-7.1 7.2-7.1 18.7 0 25.9 7.2 7.1 18.7 7.1 25.9 0L256 44.2l60.2 60.2c7.1 7.1 18.7 7.2 25.9 0M256 310.8c10.1 0 18.3 8.2 18.3 18.3v164.5c0 10.1-8.2 18.3-18.3 18.3s-18.3-8.2-18.3-18.3V329.1c0-10.1 8.2-18.3 18.3-18.3"
-										style="fill:#e2e2e2;fill-opacity:1"
-										transform="matrix(0 .0319 -.02735 0 14 -1.168)" /><path
-										class="st0"
-										d="M342.1 407.6c7.1 7.1 7.2 18.7 0 25.9L269 506.6c-7.1 7.1-18.7 7.2-25.9 0L170 433.5c-7.1-7.2-7.1-18.7 0-25.9 7.2-7.1 18.7-7.1 25.9 0l60.2 60.2 60.2-60.2c7-7.1 18.6-7.2 25.8 0"
-										style="fill:#e2e2e2;fill-opacity:1"
-										transform="matrix(0 .0319 -.02735 0 14 -1.168)" /></g
-								></svg>
+							<UnfoldVerticalIcon />
 						{/if}
 					</button>
 				{/if}
@@ -309,31 +276,7 @@
 						on:mousedown|stopPropagation
 						on:pointerdown|stopPropagation
 						title="Open this window in a new tab">
-						<svg
-							width="14"
-							height="14"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg">
-							<path
-								d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round" />
-							<path
-								d="M15 3H21V9"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round" />
-							<path
-								d="M10 14L21 3"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round" />
-						</svg>
+						<ExternalLinkIcon />
 					</button>
 				{/if}
 
